77636305a3bae60480d6fd43f7266fc800752b82
SageScroll18144 <ffss@cin.ufpe.br>
2023-10-12 00:50:43 -0300

    implementando a abertura da camera

--- frontend/app-eye-tracking/App.js
+++ frontend/app-eye-tracking/App.js
@@ -1,20 +1,59 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { Camera } from 'expo-camera'
 
 export default function App() {
+
+  const [type, setType] = useState(Camera.Constants.Type.front);
+  const [hasPermission, setHasPermission] = useState(null);
+
+  useEffect(() => {
+    (async () => {
+      const {status} = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === 'granted');
+    })();
+  }, []);
+
+  if(hasPermission === null) {
+    return <View/>;
+  }
+
+  if(hasPermission === false){
+    return <Text> *Acesso negado* </Text>
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <Camera
+        style={{flex: 1 }}
+        type={type}
+      >
+        <View style={{flex: 1 , backgroundColor:'transparent', flexDirection:'row'}}>
+          <TouchableOpacity
+            style={{
+              position: 'absolute', 
+              bottom: 20, 
+              left: 20, 
+            
+            }}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back ? Camera.Constants.Type.front : Camera.Constants.Type.back
+              );
+            }}
+            >
+            <Text style={{fontSize: 20, marginBottom: 13, color: '#FFF'}}>Swap</Text>
+          </TouchableOpacity>
+        </View>
+      </Camera>
+    </SafeAreaView >
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
   },
 });
